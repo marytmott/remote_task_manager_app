3cfd835a60697f88fb6a9b815c81c74ddf2a35ed
Mary Mott <mott.maryt@gmail.com>
2016-02-28 03:50:32 +0000

    parsing '/proc/stat' file for cpu info

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var http = require('http');
+// var http = require('http');
 var fs = require('fs');
-var PORT = 8080;
+// var PORT = 8080;
 
 function readInterval() {
-  fs.readFile('/proc/loadavg', 'utf8', function(err, data) {
+  fs.readFile('/proc/stat', 'utf8', function(err, data) {
     if (err) {
       console.log(err);
     }
-    var dataArr = data.split(' ');
-    var pcLoadNow = parseInt(dataArr[0]);
+    // var dataArr = data.split(' ');
+    var dataArr = data.split('\n');
+    var cpuLine;
+    var cpuLineArr;
+    // var pcLoadNow = parseInt(dataArr[0]);
 
     // round to whole number
     // if (pcLoadNow > 0 && pcLoadNow < 1) {
@@ -17,8 +20,16 @@ function readInterval() {
     // } else {
     //   pcLoadNow = Math.ceil(pcLoadNow);
     // }
-    console.log(dataArr);
-    console.log(pcLoadNow);
+
+    for (var i = 0; i < dataArr.length; i++) {
+      if (dataArr[i].indexOf('cpu ') !== -1){
+        cpuLine = dataArr[i];
+      }
+    }
+
+    cpuLineArr = cpuLine.split(' ');
+    console.log(cpuLineArr);
+    // console.log(pcLoadNow);
   });
 }
 
@@ -34,9 +45,9 @@ var intervalReading = setInterval(readInterval, 1000);
 setTimeout(stopReading, 6000);
 
 
-http.createServer(function(req, res) {
+// http.createServer(function(req, res) {
 
-});
+// });
 
 // http.listen(8080);
 
